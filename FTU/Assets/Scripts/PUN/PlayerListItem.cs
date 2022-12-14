a47073217b210f7cb2b6a90d443ba34e5bb7cfb0
Lenxs <Amegan.ruben@gmail.com>
2022-12-14 17:33:29 +0100

    team fix part1

--- FTU/Assets/Scripts/PUN/PlayerListItem.cs
+++ FTU/Assets/Scripts/PUN/PlayerListItem.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using TMPro;
+using System;
 
 public class PlayerListItem : MonoBehaviourPunCallbacks
 {
     Player player;
     [SerializeField] TMP_Text text;
-    [SerializeField] TMP_Text textTeam;
+    public TMP_Text textTeam;
     public TMP_Dropdown perso;
     public void SetUp(Player _player)
     {
         player = _player;
         text.text = _player.NickName;
         PlayerPrefs.SetInt("persoID", perso.value);
+        //var test = Enum.GetName(typeof(Team), PlayerPrefs.GetInt("Teams")); 
+        textTeam.text = Enum.GetName(typeof(Team), PlayerPrefs.GetInt("Teams")); 
+
     }
 
     public override void OnPlayerLeftRoom(Player otherPlayer)
