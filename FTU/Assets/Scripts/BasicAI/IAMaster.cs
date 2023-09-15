996e25bf6446f49550b1af2f1836b6f6947c798f
Kyllian Marie-Magdelaine <kmariemagdelaine@myges.fr>
2023-09-15 19:20:49 +0200

    DLL ML Importée et fonctionnelle

--- FTU/Assets/Scripts/BasicAI/IAMaster.cs
+++ FTU/Assets/Scripts/BasicAI/IAMaster.cs
@@ -1,38 +1,113 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
 
+internal static class NativeMethods
+{
+    [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
+    internal static extern IntPtr LoadLibrary(
+        string lpFileName
+    );
+}
+
+
+
 public class IAMaster : MonoBehaviour
 {
-    [DllImport("CppLib.dll",  SetLastError = true, CharSet = CharSet.Unicode)]
+    private static IntPtr lib;
+    
+    public static void LoadNativeDll(string FileName)
+    {
+        if (lib != IntPtr.Zero)
+        {
+            return;
+        }
+
+        lib = NativeMethods.LoadLibrary(FileName);
+        if (lib == IntPtr.Zero)
+        {
+            throw new Win32Exception();
+        }
+    }
+    
+    [DllImport("Assets/Scripts/BasicAI/CppLib.dll")]
     public static extern IntPtr createPMC(IntPtr npl, int L);
 
-    [DllImport("CppLib.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    [DllImport("Assets/Scripts/BasicAI/CppLib.dll")]
     public static extern void checkPMC(IntPtr pmc);
 
 
-    [DllImport("CppLib.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+    [DllImport("Assets/Scripts/BasicAI/CppLib.dll")]
     public static extern void trainPMC(IntPtr pmc, int nb_rep, float step, IntPtr X, IntPtr Y, int xRow, int xCol,
         int yCol, bool is_classification);
+    
+    [DllImport("Assets/Scripts/BasicAI/CppLib.dll")]
+    public static extern void savePMC(IntPtr pmc, char[] filename);
+
+    [DllImport("Assets/Scripts/BasicAI/CppLib.dll")]
+    public static extern IntPtr predictPMC(IntPtr pmc, IntPtr X, bool isClassification);
     
     // Start is called before the first frame update
     void Start()
     {
-        int[] npl = new int[] {2, 2};
+        LoadNativeDll("D:/Cours/PA/ForTheUniverse/FTU/Assets/Scripts/BasicAI/CppLib.dll");
+        
+        try
+        {
+            int[] npl = new int[] {2, 1};
        
-        GCHandle handle = GCHandle.Alloc(npl, GCHandleType.Pinned);
+            GCHandle handle = GCHandle.Alloc(npl, GCHandleType.Pinned);
         
-        IntPtr pointer = handle.AddrOfPinnedObject();
+            IntPtr pointer = handle.AddrOfPinnedObject();
         
         
-        IntPtr pmc = createPMC(pointer, 2);
+            IntPtr pmc = createPMC(pointer, 2);
+            Debug.Log(pmc);
+            
+            checkPMC(pmc);
+            
+            print("test");
+
+            float[] x = new float[] {0,0,1,1 };
+            float[] y = new float[] { -1, 1 };
+            
+            float[] pred = new float[] {1,1};
         
-        trainPMC(pmc, 10000, 0.01f, );
+            GCHandle handleX = GCHandle.Alloc(x, GCHandleType.Pinned);
         
-        //checkPMC(pmc);
+            IntPtr pointerX = handleX.AddrOfPinnedObject();
+        
+            GCHandle handleY = GCHandle.Alloc(y, GCHandleType.Pinned);
+        
+            IntPtr pointerY = handleY.AddrOfPinnedObject();
+            
+            GCHandle handlePred = GCHandle.Alloc(pred, GCHandleType.Pinned);
+        
+            IntPtr pointerPred = handlePred.AddrOfPinnedObject();
+        
+            trainPMC(pmc, 10000, 0.01f, pointerX, pointerY, 2, 2, 1, true );
+            
+            
+            IntPtr res = predictPMC(pmc, pointerPred, true);
+
+            double[] prediction = new double[1];
+
+            Marshal.Copy(res, prediction, 0, 1);
+            
+            Debug.Log(prediction[0]);
+
+            savePMC(pmc, "./test.txt".ToCharArray());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
     }
 
     // Update is called once per frame
